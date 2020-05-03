3db58b612c6499fa99d7d98f9e88601048aaf2a0
Bruno Lucas <bruno.melo@b2wdigital.com>
2020-05-03 00:31:52 -0300

    Adicionando drawer "menu"

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/screens/tabs_screen.dart';
 import 'package:meals/utils/app_routes.dart';
+import 'package:meals/widgets/settings_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.CATEGORIES_MEALS: (context) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (context) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (context) => SettingsScreen(),
       },
       onUnknownRoute: (settings) =>
           MaterialPageRoute(builder: (context) => CategoriesScreen()),
